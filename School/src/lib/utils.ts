aef33a31772f5823d97fcd0a14a37101817a29ab
Dhanush-Kovilapu <123237548+Dhanush9866@users.noreply.github.com>
2025-09-17 14:40:02 +0530

    Update utils.ts

--- School/src/lib/utils.ts
+++ School/src/lib/utils.ts
@@ -5,4 +5,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// NOTE: Network helpers moved to `lib/api.ts`. Import { apiRequest, API_BASE_URL } from "@/lib/api" instead.
